1c02c96b3da8efed9a6bd4a1406c3fd830a5a4f2
kunori-kiku <rokuyanlin@gmail.com>
2024-12-28 05:48:08 +0800

    Restore await

--- sing-box.js
+++ sing-box.js
@@ -6,7 +6,7 @@ const compatible_outbound = {
 
 let compatible
 let config = JSON.parse($files[0])
-let proxies = produceArtifact({
+let proxies = await produceArtifact({
   name,
   type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
   platform: 'sing-box',
