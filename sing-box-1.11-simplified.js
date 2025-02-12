d104ab3b4baebd0aa42e136232a98c4e51aa3e82
kunori-kiku <rokuyanlin@gmail.com>
2025-02-13 03:55:45 +0800

    New simplified script

---
+++ sing-box-1.11-simplified.js
@@ -0,0 +1,23 @@
+const { type, name } = $arguments
+
+let config = JSON.parse($files[0])
+let proxies = await produceArtifact({
+  name,
+  type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
+  platform: 'sing-box',
+  produceType: 'internal',
+})
+
+config.outbounds.push(...proxies)
+
+config.outbounds.map(i => {
+  if (['proxy'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /./))
+  }
+})
+
+$content = JSON.stringify(config, null, 2)
+
+function getTags(proxies, regex) {
+  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+}
